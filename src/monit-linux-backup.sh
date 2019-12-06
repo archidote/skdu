813a8b61a3f333a9450e57d656efd66b9948871f
brlndtech <itsgeoffreyberland@gmail.com>
2019-12-06 19:59:29 +0100

    monit-linux-backup.sh dev achieved :) + crontab project (just create, nothing more)

--- src/monit-linux-backup.sh
+++ src/monit-linux-backup.sh
@@ -16,20 +16,20 @@ case $nmap_rep in
         1)
 		if [ -d "/backup" ];then
 			read -p "Enter the ABSOLUTE PATH : " target  
-			echo -e  "\e[33m Sauvegarde en cours ... \e[0m";
+			echo -e  "\e[33m Loading ... \e[0m";
 			d=`date +%d-%m-%Y-%H-%M-%S`
 			folder="$target-$d"
-			folderFinal="$(echo $folder | cut -c2-)"
+			folderFinal="$(echo $folder | cut -c2-)" # cette action permet d'obtenir le nom sans / (pas obligatoire, mais je trouve que c'est plus propre)
 			cp -r $target /backup/$folderFinal
 			# on ne peut pas utiliser un underscore, car il efface le contenue de la variable $target ... 
 			echo -e "\e[92m-> Saved folder ($target) \nLocation : /backup/$folderFinal \e[0m";
 		else
 			mkdir /backup
 			read -p "Enter the ABSOLUTE PATH : " target  
-			echo -e  "\e[33m Sauvegarde en cours ... \e[0m";
+			echo -e  "\e[33m Loading ... \e[0m";
 			d=`date +%d-%m-%Y-%H-%M-%S`
 			folder="$target-$d"
-			folderFinal="$(echo $folder | cut -c2-)"
+			folderFinal="$(echo $folder | cut -c2-)" # cette action permet d'obtenir le nom sans / (pas obligatoire, mais je trouve que c'est plus propre)
 			cp -r $target /backup/$folderFinal
 			# on ne peut pas utiliser un underscore, car il efface le contenue de la variable $target ... 
 			echo -e "\e[92m-> Saved folder ($target) \nLocation : /backup/$folderFinal \e[0m";
@@ -41,9 +41,7 @@ case $nmap_rep in
 			echo -e  "\e[33m Loading ... \e[0m";
 			d=`date +%d-%m-%Y-%H-%M-%S`
 			nameoftargz="$target-$d"
-			nameoftargzFinal="$(echo $nameoftargz | cut -c2-)"
-			echo $nameoftargz
-			echo $nameoftargzFinal
+			nameoftargzFinal="$(echo $nameoftargz | cut -c2-)" # cette action permet d'obtenir le nom sans / (pas obligatoire, mais je trouve que c'est plus propre)
 			tar czvf $nameoftargzFinal.tar.gz $target && mv $nameoftargzFinal.tar.gz /backup
 			# on ne peut pas utiliser un underscore, car il efface le contenue de la variable $target ... 
 			echo -e "\e[92m-> Saved folder ($target) into a .tar.gz file \nLocation : /backup/$nameoftargzFinal.tar.tgz \e[0m";
@@ -54,13 +52,35 @@ case $nmap_rep in
 			echo -e  "\e[33m Loading ... \e[0m";
 			d=`date +%d-%m-%Y-%H-%M-%S`
 			nameoftargz="$target-$d"
-			nameoftargzFinal="$(echo $nameoftargz | cut -c2-)"
-			echo $nameoftargz
-			echo $nameoftargzFinal
+			nameoftargzFinal="$(echo $nameoftargz | cut -c2-)" # cette action permet d'obtenir le nom sans / (pas obligatoire, mais je trouve que c'est plus propre)
 			tar czvf $nameoftargzFinal.tar.gz $target && mv $nameoftargzFinal.tar.gz /backup
 			# on ne peut pas utiliser un underscore, car il efface le contenue de la variable $target ... 
 			echo -e "\e[92m-> Saved folder ($target) into a .tar.gz file \nLocation : /backup/$nameoftargzFinal.tar.tgz \e[0m";
 			echo -e "For decompress the tar.gz archive go into /backup and : tar -xvf $nameoftargzFinal.tar.tgz"
+		fi
+		;;
+		3)
+		if [ -d "/backup" ];then
+			read -p "Enter the ABSOLUTE PATH : " target  
+			echo -e  "\e[33m Loading ... \e[0m";
+			d=`date +%d-%m-%Y-%H-%M-%S`
+			folder="$target-$d"
+			folderFinal="$(echo $folder | cut -c2-)" # cette action permet d'obtenir le nom sans / (pas obligatoire, mais je trouve que c'est plus propre)
+			zip -r /backup/$folderFinal.zip $target
+			# on ne peut pas utiliser un underscore, car il efface le contenue de la variable $target ... 
+			echo -e "\e[92m-> Saved folder ($target) \nLocation : /backup/$folderFinal.zip \e[0m";
+			echo -e "For decompress the .zip archive go into /backup and : unzip $folderFinal.zip "
+		else
+			mkdir /backup
+			read -p "Enter the ABSOLUTE PATH : " target  
+			echo -e  "\e[33m Loading ... \e[0m";
+			d=`date +%d-%m-%Y-%H-%M-%S`
+			folder="$target-$d"
+			folderFinal="$(echo $folder | cut -c2-)" # cette action permet d'obtenir le nom sans / (pas obligatoire, mais je trouve que c'est plus propre)
+			zip -r /backup/$folderFinal.zip $target
+			# on ne peut pas utiliser un underscore, car il efface le contenue de la variable $target ... 
+			echo -e "\e[92m-> Saved folder ($target) \nLocation : /backup/$folderFinal.zip \e[0m";
+			echo -e "For decompress the .zip archive go into /backup and : unzip $folderFinal.zip "
 		fi
 		;;
 		4)
--- src/monit-linux-verification-paquets.sh
+++ src/monit-linux-verification-paquets.sh
@@ -2,8 +2,8 @@
 clear
 read -p "Do you want to install  htop / curl / sudo / nmap / net-tools ? (Allow the programme to function effectively) : (O/N)" rep1
     if [ $rep1 == "O" ] || [ $rep1 == "o" ]; then
-       apt-get install curl htop sudo nmap net-tools
-       echo -e "\e[92m  Your machine have installed (or updated) curl / htop / sudo / nmap / net-tools. Sconfig-linux will work more effectively \e[0m "
+       apt-get install curl htop sudo nmap net-tools zip unzip
+       echo -e "\e[92m  Your machine have installed (or updated) curl / htop / sudo / nmap / net-tools / zip / unzip  . Sconfig-linux will work more effectively \e[0m "
     else 
        echo -e " \e[91m You have canceled the packets checks.  I hope that your system have : htop / curl / sudo / nmap / net-tools \e[0m"
     fi
