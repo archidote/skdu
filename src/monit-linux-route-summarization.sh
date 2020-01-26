a2cf68d68dd852725f0f5720ae1eb33cda4dcd9f
brlndtech <itsgeoffreyberland@gmail.com>
2020-01-26 11:19:42 +0100

    Update monit-linux-route-summarization.sh

--- src/monit-linux-route-summarization.sh
+++ src/monit-linux-route-summarization.sh
@@ -10,14 +10,16 @@ function log2 {
 }
 while [ $rep = 'O' ] || [ $rep = 'o' ]
 do
-  clear
+	clear
   read -p "Entrer une @ ipv4 (addr general) ex 192.168.1.0 : " ipv4
   read -p "Entrer votre masque de base (CIDR) : " cidr
   if [ $cidr -ge 8 ] && [ $cidr -le 16 ]; then 
     ip_1octet=`echo $ipv4 | cut -f1 -d.`
     ip_2octet=`echo $ipv4 | cut -f2 -d.`
     ip_cut="$ip_1octet.$ip_2octet.0.0"
-    read -p "Quel est votre pas (ex : [1/2/4/8/16/32/64/128]) : " pas #1
+		let " pasExemple = 16 - $cidr "
+		let " pasExemple = 2 ** $pasExemple"
+    read -p "Quel est votre pas ($pasExemple semble être le plus approprié) : " pas
     read -p "Combien voulez vous de sous réseaux ? (ex : 2) : " cmb_sr #4
     let " pasmax = cmb_sr * pas "
     j=1
@@ -33,7 +35,7 @@ do
 			let " newmaskCidr = 16 - $x "
 			echo -e "\nNouveau masque permettant l'agrégation : /$newmaskCidr \nRéseau agrégée : $ip_cut/$newmaskCidr"
 			let " exposant = $newmaskCidr - 8  "
-			let " res = 2 ** exposant "
+			let " res = 2 ** $exposant "
 			let " pas = 256 / $res "
 			let " dernier = pas - 1"
 			let " netmaskDecimal = 256 - $pas "
@@ -48,8 +50,10 @@ do
     ip_2octet=`echo $ipv4 | cut -f2 -d.`
     ip_3octet=`echo $ipv4 | cut -f3 -d.`
     ip_cut="$ip_1octet.$ip_2octet.$ip_3octet.0"
-    read -p "Quel est votre pas (ex : [1/2/4/8/16/32/64/128]) : " pas # 1
-    read -p "Combien voulez vous de sous réseaux ? (ex : 2) : " cmb_sr # 4 
+		let " pasExemple = 24 - $cidr "
+		let " pasExemple = 2 ** $pasExemple"
+    read -p "Quel est votre pas ($pasExemple semble être le plus approprié) : " pas
+    read -p "Combien voulez vous de sous réseaux ? (ex : 2) : " cmb_sr
     let " pasmax = cmb_sr * pas " # 4 * 1 = 4 
     j=1
 	  if [ $pasmax -ge 1 ] && [ $pasmax -le 256 ]; then
@@ -64,14 +68,14 @@ do
       let " newmaskCidr = 24 - $x "
       echo -e "\nNouveau masque permettant l'agrégation : /$newmaskCidr \nRéseau agrégée : $ip_cut/$newmaskCidr "
       let " exposant = $newmaskCidr - 16  "
-      let " res = 2 ** exposant "
+      let " res = 2 ** $exposant "
       let " pas = 256 / $res "
       let " dernier = $pas - 1"
       let " netmaskDecimal = 256 - $pas "
       let " wildcard = $pas - 1 "
       echo -e "-> /$newmaskCidr en décimal pointé = 255.255.$netmaskDecimal.0 \n-> Wildcard mask = 0.0.$wildcard.255"
       read -p "Saisir de nouveau ? (O/N) : " rep
-	  else
+		else
       read -p "L'ordre de grandeur n'est pas respécté (>256) ! `echo $'\n->'` Saisir de nouveau ? (O/N) : " rep
     fi
   elif [ $cidr -gt 24 ] && [ $cidr -lt 32 ]; then
@@ -80,7 +84,9 @@ do
     ip_3octet=`echo $ipv4 | cut -f3 -d.`
     ip_4octet=`echo $ipv4 | cut -f4 -d.`
     ip_cut="$ip_1octet.$ip_2octet.$ip_3octet.$ip_4octet"
-    read -p "Quel est votre pas (ex : [1/2/4/8/16/32/64/128]) : " pas
+		let " pasExemple = 32 - $cidr "
+		let " pasExemple = 2 ** $pasExemple"
+    read -p "Quel est votre pas ($pasExemple semble être le plus approprié) : " pas
     read -p "Combien de sous réseaux ? : " cmb_sr
     let " pasmax = cmb_sr * pas "
     j=1
@@ -96,13 +102,13 @@ do
       let " newmaskCidr = 32 - $x "
       echo -e "\nNouveau masque permettant l'agrégation : /$newmaskCidr \nRéseau agrégée : $ip_cut/$newmaskCidr"
       let " exposant = $newmaskCidr - 24  "
-      let " res = 2 ** exposant "
+      let " res = 2 ** $exposant "
       let " pas = 256 / $res "
       let " dernier = $pas - 1"
       let " netmask = 256 - $pas "
       let " wildcard = $pas - 1 "
       echo -e "-> /$newmaskCidr en décimal pointé = 255.255.255.$netmask \n-> Wildcard mask = 0.0.0.$wildcard"
-      read -p "Saisir de nouveau ? (O/N): " rep
+		  read -p "Saisir de nouveau ? (O/N): " rep
 		else 
       read -p "L'ordre de grandeur n'est pas respécté (>256) ! `echo $'\n->'` Saisir de nouveau ? (O/N) : " rep
     fi
@@ -112,4 +118,3 @@ do
   fi
 done
 echo "Bye bye"
-clear
