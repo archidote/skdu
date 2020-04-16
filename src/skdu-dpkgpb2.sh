6c1a41de050e6eeb8feea7b346e44378d2451484
Brlndtech <38756128+brlndtech@users.noreply.github.com>
2020-04-16 17:58:20 +0200

    Update skdu-dpkgpb2.sh

--- src/skdu-dpkgpb2.sh
+++ src/skdu-dpkgpb2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 read -p "Enter the name of the blocking packet (Ex : apache2)" packet
-clear
 rm /var/lib/dpkg/info/$packet* -f
 echo -e "Action done ! "
 echo -e "\e[93m Check if the problem has been resolved, with apt update && apt upgrade (option 3) \e[0m"
