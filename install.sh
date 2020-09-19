f68f51cf12c24d589c6c033c8151f16e474b24b8
Brlndtech <itsgeoffreyberland@gmail.com>
2020-09-19 10:15:22 +0200

    fix the path of the alias skdu

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ git clone https://github.com/brlndtech/skdu
 mv skdu /etc
 cat >> $HOME/.bashrc << EOF
 # alias added by bestAliasLinux (Brlndtech Edit)  
-alias 1='apt update && apt upgrade'
+alias skdu='/etc/skdu/skdu'
 EOF
 source $HOME/.bashrc
 echo -e "\e[92mDone, please open a new terminal tab, and run \"skdu\" cmd\e[39m"
