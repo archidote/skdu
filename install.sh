be12735991a46f83e67d96446321dc7d5b4c565d
Brlndtech <itsgeoffreyberland@gmail.com>
2020-09-19 10:07:08 +0200

    install.sh has been added, and / of skdu has moved to /etc

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+git clone https://github.com/brlndtech/skdu
+mv skdu /etc
+cat >> $HOME/.bashrc << EOF
+# alias added by bestAliasLinux (Brlndtech Edit)  
+alias 1='apt update && apt upgrade'
+EOF
+source $HOME/.bashrc
+echo -e "\e[92mDone, please open a new terminal tab, and run \"skdu\" cmd\e[39m"
